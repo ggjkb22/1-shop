c7393d49f2e697f9e04797a860b541d54e94a9d8
qrj <923207736@qq.com>
2020-11-29 19:38:04 +0800

    commit

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -63,15 +63,41 @@
         </el-card>
         <!-- 添加用户对话框 -->
         <el-dialog
-            title="提示"
+            title="添加用户"
             :visible.sync="addDialogVisible"
-            width="50%">
-            <!-- 内容主题区域 -->
-            <span>这是一段信息</span>
+            width="50%"
+            @close="addDialogClosed">
+            <!-- 内容主体区域 -->
+            <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+                <el-form-item label="用户名" prop="username" >
+                    <el-input v-model="addForm.username" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="addForm.password" show-password clearable></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email" >
+                    <el-input v-model="addForm.email" clearable></el-input>
+                </el-form-item>
+                <el-form-item label="手机" prop="mobile">
+                    <el-input v-model="addForm.mobile" clearable></el-input>
+                </el-form-item>
+            </el-form>
             <!-- 底部区域 -->
             <span slot="footer" class="dialog-footer">
                 <el-button @click="addDialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+                <el-button type="primary" @click="addUser">确 定</el-button>
+            </span>
+        </el-dialog>
+        <!-- 修改用户对话框 -->
+        <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="30%"
+            :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -80,16 +106,61 @@
 <script>
 export default {
     data(){
+        //自定义表单校验规则
+        const checkEmail=(rule,value,callback)=>{
+            const regEmail = /^[\w\-]+@[\w\-]+\.(com|org|cn|cc|net)$/i;
+            if(regEmail.test(value)){
+                //合法的邮箱
+                return callback()
+            }else{
+                callback(new Error("该邮箱不合法"))
+            }
+        };
+        const checkMobile = (rule,value,callback)=>{
+            const regMobile = /^1[3|4|5|7|8][0-9]{9}$/;
+            if(regMobile.test(value)){
+                return callback()
+            }else{
+                callback(new Error("该手机号不合法"))
+            }
+        };
+        
         return{
             queryInfo:{
                 query:"",
                 pagenum:1,
-                pagesize:2
+                pagesize:5
             },
             userList:[],
             userTotal:0,
             //控制添加用户的对话框是否隐藏
-            addDialogVisible:false
+            addDialogVisible:false,
+            /* 添加用户表单数据 */
+            addForm:{
+                username:"",
+                password:"",
+                email:"",
+                mobile:""
+            },
+            /* 添加表单验证规则 */
+            addFormRules:{
+                username:[
+                    {required:true,message:"请输入用户名",trigger:"blur"},
+                    {min:3,max:10,message:"用户名长度为 3~10 个字符",trigger:"blur"}
+                ],
+                password:[
+                    {required:true,message:"请输入密码",trigger:"blur"},
+                    {min:6,max:15,message:"密码长度为 6~15 个字符",trigger:"blur"}
+                ],
+                email:[
+                    {required:true,message:"请输入邮箱",trigger:"blur"},
+                    {validator:checkEmail,trigger:"blur"},
+                ],
+                mobile:[
+                    {required:true,message:"请输入手机号",trigger:"blur"},
+                    {validator:checkMobile,trigger:"blur"},
+                ]
+            }
         }
     },
     created(){
@@ -104,7 +175,6 @@ export default {
             if(res.meta.status !==200) return this.$message.error("用户列表获取失败");
             this.userList = res.data.users;
             this.userTotal = res.data.total;
-            console.log("hello")
         },
         /* 监听 pagesize 的改变 */
         handleSizeChange(newPage){
@@ -133,6 +203,21 @@ export default {
             let mon = date.getMonth()+1;
             let day = date.getDate();
             return year + "." + mon + "." + day
+        },
+        /* 监听添加用户对话框关闭时的事件 */
+        addDialogClosed(){
+            this.$refs.addFormRef.resetFields();
+        },
+        /* 添加用户按钮事件 */
+        addUser(){
+            this.$refs.addFormRef.validate(async valid=>{
+                if(!valid) return ;
+                const {data:res} = await this.$http.post("users",this.addForm);
+                if(res.meta.status!=201) return this.$message.error(res.meta.msg);
+                this.addDialogVisible = false;
+                this.$message.success("用户创建成功");
+                this.getUserList();
+            })
         }
     } 
 }
